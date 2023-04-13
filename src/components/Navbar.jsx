da925244a31fd4a9a3380bc7647197c32bac6b8b
LuisBrodely <luisbrodely2@gmail.com>
2023-04-13 14:22:44 -0600

    Tarea

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
-import Link from 'next/link'
 import ActiveLink from './ActiveLink'
+
+const menuItems = [
+  {
+      text: 'Home',
+      href: '/'
+  },
+  {
+      text: 'About',
+      href: '/about'
+  },
+  {
+      text: 'Contact',
+      href: '/contact'
+  },
+  {
+      text: 'Pricing',
+      href: '/pricing'
+  },
+];
 
 export default function Navbar() {
   return (
     <nav>
-        <ActiveLink text='home' href='/' />
-        <ActiveLink text='about' href='/about' />
-        <ActiveLink text='contact' href='/contact' />
+      {menuItems.map((item, index) => (
+        <ActiveLink text={item.text} href={item.href} key={index}/>
+      ))}
     </nav>
   )
 }
---
+++ src/pages/pricing/index.jsx
@@ -0,0 +1,10 @@
+import MainLayout from '@/components/layouts/MainLayout'
+
+export default function Pricing() {
+  return (
+    <MainLayout>
+      <h2 className="text-3xl font-bold">Este es el Pricing</h2>
+    </MainLayout>
+  )
+}
+
